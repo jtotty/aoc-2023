64e4764e422635adea1d74a021d9b9f1561beaec
jtotty <jt.totty1991@gmail.com>
2023-12-12 23:13:41 +0000

    Day 04 - Part 02

--- typescript/src/p4.ts
+++ typescript/src/p4.ts
@@ -3,11 +3,13 @@ import fs from 'fs';
 function run(fileType: string) {
     const data = fs.readFileSync(fileType, 'utf-8').trim().split('\n');
 
-    const total01 = parseDataPart1(data);
-    console.log(total01);
+    // const total01 = parseDataPart1(data);
+    // console.log(total01);
 
+    const tick02 = performance.now();
     const total02 = parseDataPart2(data);
     console.log(total02);
+    console.log(`That took ${performance.now() - tick02} ms`);
 }
 
 function parseDataPart1(cards: string[]): number {
@@ -17,22 +19,60 @@ function parseDataPart1(cards: string[]): number {
         const parts = card.split(/: |\| /g);
         const winningNumbers = parts[1].match(/\d+/g);
         const numbers = parts[2].match(/\d+/g);
-        let tally = 0;
+        let count = 0;
 
-        winningNumbers?.forEach((num) => {
-            if (numbers?.includes(num)) {
-                tally++;
-            }
-        });
+        if (winningNumbers && numbers) {
+            winningNumbers.forEach((num) => {
+                if (numbers.includes(num)) {
+                    count++;
+                }
+            });
+        }
 
-        total += (tally !== 0) ? Math.pow(2, tally - 1) : 0;
+        total += count !== 0 ? Math.pow(2, count - 1) : 0;
     });
 
     return total;
 }
 
 function parseDataPart2(cards: string[]): number {
-    return 0;
+    let total = 0;
+    const cardCopies: Record<number, number> = Object.create(null);
+
+    cards.forEach((card, index) => {
+        const parts = card.split(/: |\| /g);
+        const winningNumbers = parts[1].match(/\d+/g);
+        const playerNumbers = parts[2].match(/\d+/g);
+
+        let count = 0;
+
+        if (winningNumbers && playerNumbers) {
+            winningNumbers.forEach((winningNum) => {
+                if (playerNumbers.includes(winningNum)) {
+                    count++;
+                }
+            });
+        }
+
+        cardCopies[index + 1] = count;
+    });
+
+    function countCopies(item: number) {
+        total++;
+        const itemCount = cardCopies[item];
+
+        if (itemCount) {
+            for (let i = item + 1; i <= item + itemCount; i++) {
+                countCopies(i);
+            }
+        }
+    }
+
+    for (let key of Object.keys(cardCopies)) {
+        countCopies(Number(key));
+    }
+
+    return total;
 }
 
 run('../data/p4/data.txt');
